c5a8cbb9057be6351045836ced9ee338006281ac
zattic <741372445@qq.com>
2018-12-25 15:25:52 +0800

    1225
    new

--- WebApplication1/ConsoleApplication1/Program.cs
+++ WebApplication1/ConsoleApplication1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            ALL();
+            NewMethod();
             //CreatNewPosts();
             //UpdatePosts();
             //DeletePosts();
@@ -22,137 +22,170 @@ namespace ConsoleApplication1
             Console.WriteLine("按任意键退出");
             Console.ReadKey();
         }
-        static void ALL()
+        private static void NewMethod()
         {
             //显示所有博客
+            Console.WriteLine("所有博客：");
             QueryBlog();
-            Console.WriteLine(" --1-新增博客   --2-更改博客  --3-删除博客  --4-操作帖子  --5-退出");
+            Console.WriteLine("-1-退出 -2-新增博客 -3-更改博客 -4-删除博客 -5-操作帖子");
             Console.WriteLine("请输入操作指令");
             int i = int.Parse(Console.ReadLine());
-            if (i == 5)
+            if (i == 1)
             {
+                Environment.Exit(0);
                 return;
             }
 
-            else if (i == 1)
+            else if (i == 2)
             {
                 createBlog();
                 QueryBlog();
-                Console.Clear();
-                ALL();
+                Console.Clear(); NewMethod();
+
             }
-            else if (i == 2)
+            else if (i == 3)
             {
                 Update();
                 QueryBlog();
                 Console.Clear();
-                ALL();
+                NewMethod();
+
             }
-            else if (i == 3)
+            else if (i == 4)
             {
                 Delete();
                 QueryBlog();
                 Console.Clear();
-                ALL();
+                NewMethod();
+
             }
-            else if (i == 4)
+            else if (i == 5)
             {
-                int blogid = GetBlogId();
+                int blogid = GeBlogId();
+                Console.WriteLine(blogid);
                 //显示指定博客的帖子列表
                 DisplayPosts(blogid);
+                //清楚控制台信息
+                Console.Clear();
+                Console.WriteLine("-0-返回上一层 -1-新增帖子 -2-更改帖子 -3-删除帖子");
+                int A = int.Parse(Console.ReadLine());
+                if (A == 0)
+                {
+                    NewMethod();
+                    Console.Clear();
+                    Console.WriteLine("所有博客：");
+                    QueryBlog();
+                    Console.WriteLine("-1-退出 -2-新增博客 -3-更改博客 -4-删除博客 -5-操作帖子");
+                    return;
+                }
+                else if (A == 1)
+                {
+                    CreatNewPosts(blogid);
+                    DisplayPosts(blogid);
+                    Console.WriteLine("新增成功");
+                }
+                else if (A == 2)
+                {
+                    UpdatePosts();
+                    DisplayPosts(blogid);
+                    Console.WriteLine("修改成功");
+
+                }
+                else if (A == 3)
+                {
+                    DeletePosts();
+                    DisplayPosts(blogid);
+                    Console.WriteLine("删除成功");
+                }
             }
             else
             {
                 Console.WriteLine("无效字符");
+                return;
             }
-
         }
-        static int GetBlogId()
+       
+        static int GeBlogId()
         {
-            //提示用户输入博客id
-            Console.WriteLine("请输入要操作的博客id");
-            //获取用户输入，并存储到变量id
+            Console.WriteLine("请输入ID：");
             int id = int.Parse(Console.ReadLine());
-            //返回id
             return id;
         }
         //新增博客
         static void createBlog()
         {
-            Console.WriteLine("请输入一个博客名称");         
-            string name = Console.ReadLine();
-            Blog blog = new Blog();
-            blog.Name = name;
-            BlogBusinessLayerss bbl= new BlogBusinessLayerss();
-            bbl.Add(blog);
+                Console.WriteLine("请输入一个博客名称");
+                string name = Console.ReadLine();
+                Blog blog = new Blog();
+                blog.Name = name;
+                BlogBusinessLayerss bbl = new BlogBusinessLayerss();
+                bbl.Add(blog);
 
-        }
-        //查询全部博客
-        static void QueryBlog()
-        {
-              
-            BlogBusinessLayerss bbl = new BlogBusinessLayerss();
-            var blogs = bbl.Query();
-            foreach(var item in blogs)
-            {
-                Console.WriteLine(item.BlogId + "" + item.Name);
             }
-        }
-        //修改博客
-        static void Update()
+            //查询全部博客
+            static void QueryBlog()
         {
-            Console.WriteLine("请输入id");
-            int id = int.Parse(Console.ReadLine());
-            BlogBusinessLayerss bbl = new BlogBusinessLayerss();
-            Blog blog= bbl.Query(id);
-            Console.WriteLine("请输入新名字");
-            string name = Console.ReadLine();
-            blog.Name = name;
-            bbl.Update(blog);
-        }
-        //删除博客
-        static void Delete()
+
+                BlogBusinessLayerss bbl = new BlogBusinessLayerss();
+                var blogs = bbl.Query();
+                foreach (var item in blogs)
+                {
+                    Console.WriteLine(item.BlogId + "" + item.Name);
+                }
+            }
+            //修改博客
+            static void Update()
         {
-            BlogBusinessLayerss bbl = new BlogBusinessLayerss();
-            Console.WriteLine("请输入一个博客ID");
-            int id = int.Parse(Console.ReadLine());
-            Blog blog = bbl.Query(id);
-            bbl.Delete(blog);
-        }
-
-        static void AddPost()
+                Console.WriteLine("请输入id");
+                int id = int.Parse(Console.ReadLine());
+                BlogBusinessLayerss bbl = new BlogBusinessLayerss();
+                Blog blog = bbl.Query(id);
+                Console.WriteLine("请输入新名字");
+                string name = Console.ReadLine();
+                blog.Name = name;
+                bbl.Update(blog);
+            }
+            //删除博客
+            static void Delete()
         {
-            //显示博客列表
-            QueryBlog();
-            //用户选择某个博客（id）
-            Console.WriteLine("输入一个博客ID");
-            int id = int.Parse(Console.ReadLine());
-
-            //显示指定博客的帖子列表
-            DisplayPosts(id);
-
-            //根据指定到博客信息创建新帖子  
-            Console.WriteLine("                              --新建帖子--");
-            CreatNewPosts();
-
-            //显示指定博客的帖子列表
-            DisplayPosts(id);
-        }
-        static void DisplayPosts(int blogID)
-        {
-                       
-            BlogBusinessLayerss bbl = new BlogBusinessLayerss();
-            Blog blog = bbl.Query(blogID);
-
-            List<Post> postList = bbl.pQuery(blogID);
-            foreach (var item in postList)
-            {
-                Console.WriteLine("博客ID:{0}  ---  帖子题目：{1}  ---  帖子内容:{2}  ---帖子ID:{3}",item.BlogId, item.Title,item.Content,item.PostId);
+                BlogBusinessLayerss bbl = new BlogBusinessLayerss();
+                Console.WriteLine("请输入一个博客ID");
+                int id = int.Parse(Console.ReadLine());
+                Blog blog = bbl.Query(id);
+                bbl.Delete(blog);
             }
 
-        }
-        //新增帖子
-        static void CreatNewPosts()
+            static void AddPost()
+        {
+                //显示博客列表
+                QueryBlog();
+                //用户选择某个博客（id）
+                Console.WriteLine("输入一个博客ID");
+                int id = int.Parse(Console.ReadLine());
+
+                //显示指定博客的帖子列表
+                DisplayPosts(id);
+
+                //根据指定到博客信息创建新帖子  
+                Console.WriteLine("                              --新建帖子--");               
+
+                //显示指定博客的帖子列表
+                DisplayPosts(id);
+            }
+            static void DisplayPosts(int blogID)
+        {
+
+                BlogBusinessLayerss bbl = new BlogBusinessLayerss();
+                Blog blog = bbl.Query(blogID);
+
+                List<Post> postList = bbl.pQuery(blogID);
+                foreach (var item in postList)
+                {
+                    Console.WriteLine("博客ID:{0}  ---  帖子题目：{1}  ---  帖子内容:{2}  ---帖子ID:{3}", item.BlogId, item.Title, item.Content, item.PostId);
+                }
+            }
+            //新增帖子
+        static void CreatNewPosts(int blogid)
         {
             Console.WriteLine("请输入一个博客ID");
             int id = int.Parse(Console.ReadLine());
@@ -169,7 +202,6 @@ namespace ConsoleApplication1
             BlogBusinessLayerss bbl = new BlogBusinessLayerss();
             bbl.pAdd(post);
         }
-
         //static void PostByID(int blogID)
         //{
         //    Console.WriteLine("请输入一个帖子名称");
@@ -188,44 +220,45 @@ namespace ConsoleApplication1
 
 
         //修改帖子
-       
         static void UpdatePosts()
         {
-            QueryBlog();
+                QueryBlog();
 
 
-            BlogBusinessLayerss bbl = new BlogBusinessLayerss();
-            Console.WriteLine("请输入一个博客ID");
-            int blogID = int.Parse(Console.ReadLine());
-            DisplayPosts(blogID);
-            Console.WriteLine("请输入要修改的帖子ID");
-            int postID = int.Parse(Console.ReadLine());
-            Post post = bbl.postQuery(postID);
-            Console.WriteLine("请输入新题目");
-            string newTitle = Console.ReadLine();
-            post.Title = newTitle;
-            Console.WriteLine("请输入新内容");
-            string newContent = Console.ReadLine();
-            post.Content = newContent;
-            bbl.pUpdate(post);
-            DisplayPosts(blogID);
+                BlogBusinessLayerss bbl = new BlogBusinessLayerss();
+                Console.WriteLine("请输入一个博客ID");
+                int blogID = int.Parse(Console.ReadLine());
+                DisplayPosts(blogID);
+                Console.WriteLine("请输入要修改的帖子ID");
+                int postID = int.Parse(Console.ReadLine());
+                Post post = bbl.postQuery(postID);
+                Console.WriteLine("请输入新题目");
+                string newTitle = Console.ReadLine();
+                post.Title = newTitle;
+                Console.WriteLine("请输入新内容");
+                string newContent = Console.ReadLine();
+                post.Content = newContent;
+                bbl.pUpdate(post);
+                DisplayPosts(blogID);
 
 
+            }
+            //删除帖子
+            static void DeletePosts()
+        {
+
+                BlogBusinessLayerss bbl = new BlogBusinessLayerss();
+                Console.WriteLine("请输入一个博客ID");
+                int blogID = int.Parse(Console.ReadLine());
+                DisplayPosts(blogID);
+                Console.WriteLine("请输入要删除的帖子ID");
+                int postID = int.Parse(Console.ReadLine());
+                Post post = bbl.postQuery(postID);
+                bbl.DeletePost(post);
+                DisplayPosts(blogID);
+
+            }
         }
-        //删除帖子
-        static void DeletePosts()
-        {           
-        
-            BlogBusinessLayerss bbl = new BlogBusinessLayerss();
-            Console.WriteLine("请输入一个博客ID");
-            int blogID = int.Parse(Console.ReadLine());
-            DisplayPosts(blogID);
-            Console.WriteLine("请输入要删除的帖子ID");
-            int postID = int.Parse(Console.ReadLine());
-            Post post = bbl.postQuery(postID);
-            bbl.DeletePost(post);
-            DisplayPosts(blogID);
+    } 
 
-        }
-    }
-}
+
